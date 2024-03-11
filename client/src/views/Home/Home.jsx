61eb4a8d88aeb4d5488f3513e8d807ccfec30c9d
Alen Oviedo <afoviedola@gmail.com>
2024-03-11 16:48:57 -0500

    create project

---
+++ client/src/views/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div>
+            <h1>Vista para home</h1>
+        </div>
+    )
+};
+
+export default Home;
---
+++ client/src/views/Landing/Landing.jsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+    return(
+        <div>
+            <h1>Vista de presentación</h1>
+        </div>
+    )
+};
+
+export default Landing;
