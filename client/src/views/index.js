4d072f212f59bc21e51f023e28494578118d6269
Alen Oviedo <afoviedola@gmail.com>
2024-03-12 13:31:27 -0500

    routes

---
+++ client/src/views/index.js
@@ -0,0 +1,6 @@
+import Collaboration from './Collaboration/Collaboration';
+import Form from './Form/Form';
+import Home from './Home/Home';
+import Landing from './Landing/Landing';
+
+export { Collaboration, Form, Home, Landing };
