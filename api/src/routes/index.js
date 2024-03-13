6f5daa9ecfe6ecab84a9b300fde9ab6ac1afc8e9
RicardoSumoza <rsumoza04@gmail.com>
2024-03-12 23:43:03 -0400

    routes login/register

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,9 +3,9 @@ const { users } = require('../../users.json');
 
 const router = Router();
 
-router.get('/users', (req, res) => {
-    return res.send(users)
-})
-
 
 module.exports = router
+
+// ejemplo de las rutas:
+// http://localhost:3001/auth/login     ===> para el login
+// http://localhost:3001/auth/register  ===> para el register
