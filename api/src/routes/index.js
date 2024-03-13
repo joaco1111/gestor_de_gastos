7fd3a095a11fec16dfbe34412e27d501b1edad22
RicardoSumoza <rsumoza04@gmail.com>
2024-03-13 00:07:16 -0400

    routes login/register

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,12 +3,17 @@ const { users } = require('../../users.json');
 
 const router = Router();
 
+<<<<<<< Updated upstream
 router.get('/users', (req, res) => {
     return res.send(users)
 })
 
 //traigo mis rutas
 
+=======
+//traigo mis rutas
+
+>>>>>>> Stashed changes
 const authRouter = require('./authRoute')
 
 //hago el enrutado
@@ -20,4 +25,8 @@ module.exports = router
 
 // ejemplo de las rutas:
 // http://localhost:3001/auth/login     ===> para el login
+<<<<<<< Updated upstream
 // http://localhost:3001/auth/register  ===> para el register
+=======
+// http://localhost:3001/auth/register  ===> para el register
+>>>>>>> Stashed changes
