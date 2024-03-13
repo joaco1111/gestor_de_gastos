9a1a901eb4ccc2cb3f63d6a58318541aaee9def7
joaco1111 <joacoo973@gmail.com>
2024-03-13 16:04:10 -0300

    payment information

---
+++ client/src/components/Donation/DonationInfo.js
@@ -0,0 +1,14 @@
+// DonationInfo.js
+import React from 'react';
+
+const DonationInfo = () => {
+  return (
+    <div className="donation-info">
+      <h2>Gracias por tu donación</h2>
+      <p>Tu contribución ayuda a hacer del mundo un lugar mejor.</p>
+    </div>
+  );
+};
+
+export default DonationInfo;
+
