11f88ed0df5a7651f73e26cc4acddd19872b189b
Alen Oviedo <afoviedola@gmail.com>
2024-03-12 21:49:20 -0500

    fix main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,8 +7,8 @@ import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 )
