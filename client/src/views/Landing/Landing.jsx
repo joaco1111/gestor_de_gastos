9814d4534302fe92aaae462fb88296c16b538624
Alen Oviedo <afoviedola@gmail.com>
2024-03-13 20:31:04 -0500

    login visto desde Landing

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,7 +1,11 @@
-const Landing = () => {
+import Loggin from '../../components/Loggin/Loggin';
+
+const Landing = ({ login }) => {
     return(
         <div>
             <h1>Vista de presentación</h1>
+
+            <Loggin login={ login } />
         </div>
     )
 };
